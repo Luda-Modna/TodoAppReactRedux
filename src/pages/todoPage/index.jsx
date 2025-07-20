c7b70672c1e28401936dede54ff7aeaf7ee55610
Luda Modna <ludmilamodna3@gmail.com>
2025-07-20 16:18:15 +0300

    Add components + page

---
+++ src/pages/todoPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoForm from '../../components/TodoForm';
+import TodoList from '../../components/TodoList';
+
+function TodoPage () {
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
+}
+
+export default TodoPage;
