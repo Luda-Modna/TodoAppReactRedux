ab0c17615ccf27513ea258efb5f988964d13e005
Luda Modna <ludmilamodna3@gmail.com>
2025-07-21 20:30:11 +0300

    Added styles isDone/overdue todo item

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import { deleteTodo, updateTask } from '../../store/slices/tasksSlice';
+import classNames from 'classnames';
+import styles from './TodoItem.module.sass';
 
 function TodoList ({ tasks, status, overdue, deleteTodoTask, updateTodoTask }) {
   const now = new Date();
@@ -22,24 +24,36 @@ function TodoList ({ tasks, status, overdue, deleteTodoTask, updateTodoTask }) {
     return true;
   });
 
+
   return (
     <ul>
-      {filteredTasks.map(t => (
-        <li key={t.id}>
-          <label>
-            <input
-              type='checkbox'
-              checked={t.isDone}
-              onChange={({ target: { checked } }) => {
-                isChangeDone(t.id, checked);
-              }}
-            />
-          </label>
-          <p>{t.text} </p>
-          <p>Deadline: {t.deadline}</p>
-          <button onClick={() => deleteTodoTask(t.id)}>Delete todo</button>
-        </li>
-      ))}
+      {filteredTasks.map(t => {
+        const isOverdue = new Date(t.deadline) < now;
+
+        const styleName = classNames(styles.task, {
+          [styles.done]: t.isDone ,
+          [styles.overdue]: t.isDone && isOverdue,
+          [styles.notDone]: !t.isDone && !isOverdue,
+          [styles.notOverdue]: !t.isDone && isOverdue,
+        });
+
+        return (
+          <li key={t.id} className={styleName}>
+            <label>
+              <input
+                type='checkbox'
+                checked={t.isDone}
+                onChange={({ target: { checked } }) => {
+                  isChangeDone(t.id, checked);
+                }}
+              />
+            </label>
+            <p>{t.text} </p>
+            <p>Deadline: {t.deadline}</p>
+            <button onClick={() => deleteTodoTask(t.id)}>Delete todo</button>
+          </li>
+        );
+      })}
     </ul>
   );
 }
