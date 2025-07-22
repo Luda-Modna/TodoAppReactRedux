16e2a65e8d798cb637d748f3cbf018cca6d928e1
Luda Modna <ludmilamodna3@gmail.com>
2025-07-22 18:16:10 +0300

    Added weather styles + btn filter and styles overlayFilters

--- src/pages/projectPage/index.jsx
+++ src/pages/projectPage/index.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
 import Filters from '../../components/Filters';
 import TodoComponent from '../../components/TodoComponent';
 import Weather from '../../components/Weather';
-import styles from './ProjectPageStyle.module.sass'
+import styles from './ProjectPageStyle.module.sass';
 
 function ProjectPage () {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+
   return (
     <div className={styles.majorContainer}>
-      <Filters />
+      {!isFilterOpen && (
+        <button
+          className={styles.filterBtn}
+          onClick={() => setIsFilterOpen(true)}
+        >
+          Filters
+        </button>
+      )}
+      {isFilterOpen && (
+        <>
+          <div
+            className={styles.filtersOverlay}
+            onClick={() => setIsFilterOpen(false)}
+          />
+          <div className={styles.filtersModal}>
+            <button
+              className={styles.closeBtn}
+              onClick={() => setIsFilterOpen(false)}
+            >
+              ×
+            </button>
+            <Filters />
+          </div>
+        </>
+      )}
+      <aside>
+        <div className={styles.weatherStyles}>
+          <Weather />
+        </div>
+        <div className={styles.hideFilter}>
+          <Filters />
+        </div>
+      </aside>
       <TodoComponent />
-      <Weather />
     </div>
   );
 }
