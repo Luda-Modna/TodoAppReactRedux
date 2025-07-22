c174080ea20f210da0fbf49a464e1b493c25d1ed
Luda Modna <ludmilamodna3@gmail.com>
2025-07-22 17:09:00 +0300

    Added taskItem style

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { deleteTodo, updateTask } from '../../store/slices/tasksSlice';
 import classNames from 'classnames';
-import styles from './TodoItem.module.sass';
+import styles from './../../pages/projectPage/ProjectPageStyle.module.sass';
 
 function TodoList ({ tasks, status, overdue, deleteTodoTask, updateTodoTask }) {
   const now = new Date();
@@ -24,14 +24,13 @@ function TodoList ({ tasks, status, overdue, deleteTodoTask, updateTodoTask }) {
     return true;
   });
 
-
   return (
-    <ul>
+    <ul className={styles.tasksList}>
       {filteredTasks.map(t => {
         const isOverdue = new Date(t.deadline) < now;
 
         const styleName = classNames(styles.task, {
-          [styles.done]: t.isDone ,
+          [styles.done]: t.isDone,
           [styles.overdue]: t.isDone && isOverdue,
           [styles.notDone]: !t.isDone && !isOverdue,
           [styles.notOverdue]: !t.isDone && isOverdue,
@@ -39,18 +38,20 @@ function TodoList ({ tasks, status, overdue, deleteTodoTask, updateTodoTask }) {
 
         return (
           <li key={t.id} className={styleName}>
-            <label>
-              <input
-                type='checkbox'
-                checked={t.isDone}
-                onChange={({ target: { checked } }) => {
-                  isChangeDone(t.id, checked);
-                }}
-              />
-            </label>
-            <p>{t.text} </p>
-            <p>Deadline: {t.deadline}</p>
-            <button onClick={() => deleteTodoTask(t.id)}>Delete todo</button>
+            <div className={styles.taskRow}>
+              <label>
+                <input
+                  type='checkbox'
+                  checked={t.isDone}
+                  onChange={({ target: { checked } }) => {
+                    isChangeDone(t.id, checked);
+                  }}
+                />
+              </label>
+              <p className={styles.taskInfo}>{t.text} </p>
+            </div>
+            <p className={styles.taskInfo}>Deadline: {t.deadline}</p>
+            <button className={styles.deleteButton} onClick={() => deleteTodoTask(t.id)}>Delete todo</button>
           </li>
         );
       })}
